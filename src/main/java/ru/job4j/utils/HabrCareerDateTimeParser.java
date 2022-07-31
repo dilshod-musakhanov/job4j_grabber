1e6d53c12709d138ca55d932741dc99b729b9d46
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-07-31 14:25:07 +0400

    Преобразование даты [#289476 #327147]

--- src/main/java/ru/job4j/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/utils/HabrCareerDateTimeParser.java
@@ -1,6 +1,8 @@
 package ru.job4j.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -10,42 +12,7 @@ public class HabrCareerDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        int year = 0;
-        int month = 0;
-        int day = 0;
-        int hour = 0;
-        int minute = 0;
-        int second = 0;
-        Pattern p = Pattern.compile("\\d+");
-        Matcher m = p.matcher(parse);
-        List<String> numbers = new ArrayList<>();
-        while (m.find()) {
-            numbers.add((m.group()));
-        }
-        for (int i = 0; i < numbers.size(); i++) {
-            switch (i) {
-                case 0:
-                    year = Integer.parseInt(numbers.get(i));
-                    break;
-                case 1:
-                    month = Integer.parseInt(numbers.get(i));
-                    break;
-                case 2:
-                    day = Integer.parseInt(numbers.get(i));
-                    break;
-                case 3:
-                    hour = Integer.parseInt(numbers.get(i));
-                    break;
-                case 4:
-                    minute = Integer.parseInt(numbers.get(i));
-                    break;
-                case 5:
-                    second = Integer.parseInt(numbers.get(i));
-                    break;
-                default:
-                    break;
-            }
-        }
-        return LocalDateTime.of(year, month, day, hour, minute, second);
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(parse);
+        return zonedDateTime.toLocalDateTime();
     }
 }
