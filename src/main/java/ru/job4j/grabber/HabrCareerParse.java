c6e3bdc2eda9ce925bca25c1b680b9350c446904
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-07-31 07:18:04 +0400

    Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -14,20 +14,35 @@ public class HabrCareerParse {
 
     private static  final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
 
+    public static void parseAsPerPage(String url, int pages) {
+        String urlPage;
+        for (int i = 1; i <= pages; i++) {
+            urlPage = String.format("%s?page=%s", PAGE_LINK, i);
+            Connection connection = Jsoup.connect(urlPage);
+            Document document;
+            try {
+                document = connection.get();
+                System.out.printf("*** Страница № %s ***%n", i);
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    Element dateElement = row.select(".vacancy-card__date").first();
+                    Element dateEl = dateElement.child(0);
+                    String vacancyDate = dateEl.attr("datetime");
+                    Element titleElement = row.select(".vacancy-card__title").first();
+                    Element linkElement = titleElement.child(0);
+                    String vacancyName = titleElement.text();
+                    String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+                    System.out.printf("%s, Опубликовано: %s, %s%n", vacancyName, vacancyDate, link);
+                });
+                System.out.println();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
-        Document document = connection.get();
-        Elements rows = document.select(".vacancy-card__inner");
-        rows.forEach(row -> {
-            Element dateElement = row.select(".vacancy-card__date").first();
-            Element dateEl = dateElement.child(0);
-            String vacancyDate = dateEl.attr("datetime");
-            Element titleElement = row.select(".vacancy-card__title").first();
-            Element linkElement = titleElement.child(0);
-            String vacancyName = titleElement.text();
-            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.printf("%s, Опубликовано: %s, %s%n", vacancyName, vacancyDate, link);
-        });
+        parseAsPerPage(PAGE_LINK, 5);
     }
 
 }
