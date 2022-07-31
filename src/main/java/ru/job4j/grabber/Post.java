832eeefdb36407ffc77aeda22427ca9823735046
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-07-31 12:51:06 +0400

    Модель данных - Post. [#285211 #327322]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -15,6 +15,13 @@ public class Post {
 
     private LocalDateTime created;
 
+    public Post(String link, String title, String description, LocalDateTime created) {
+        this.link = link;
+        this.title = title;
+        this.description = description;
+        this.created = created;
+    }
+
     public Post(int id, String link, String title, String description, LocalDateTime created) {
         this.id = id;
         this.link = link;
